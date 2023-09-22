7723d7beb5c146b29338ec3e14d52f4b6e36e4e5
Sören Kuklau <kuklau@trenz.de>
2023-09-22 13:03:02 +0200

    add AddRazorEmailRenderer for more convenient (and correct) setup

---
+++ TRENZ.Lib.RazorMail/Extensions/RazorMailServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+
+using TRENZ.Lib.RazorMail.Services;
+
+namespace TRENZ.Lib.RazorMail.Extensions;
+
+public static class RazorMailServiceCollectionExtensions
+{
+    public static IServiceCollection AddRazorEmailRenderer(this IServiceCollection services)
+    {
+        services.AddMvcCore()
+                .AddRazorViewEngine();
+
+        services.AddTransient<IRazorEmailRenderer, RazorEmailRenderer>();
+
+        return services;
+    }
+}
