001796b7b281da2eff4f8f3cafef228f976ef4bd
Ricardo Boss <mail@ricardoboss.de>
2024-04-27 12:27:39 +0200

    Move common code to the same method

--- TRENZ.Lib.RazorMail.SampleWebApi/Controllers/MailController.cs
+++ TRENZ.Lib.RazorMail.SampleWebApi/Controllers/MailController.cs
@@ -15,17 +15,7 @@ public class MailController(
     [HttpPost]
     public async Task<IActionResult> SendWithSystemNet([FromBody] SendSampleMailRequest request, [FromKeyedServices("System.Net.Mail")] IMailClient client)
     {
-        var renderedMail = await MakeRenderedMail(request);
-
-        var message = new MailMessage
-        {
-            Content = renderedMail,
-            Headers = new()
-            {
-                From = request.From,
-                Recipients = request.To.Select(x => new MailAddress(x)),
-            },
-        };
+        var message = await MakeMessage(request);
 
         await client.SendAsync(message);
 
@@ -35,7 +25,19 @@ public class MailController(
     [HttpPost]
     public async Task<IActionResult> SendWithMailKit([FromBody] SendSampleMailRequest request, [FromKeyedServices("MailKit")] IMailClient client)
     {
-        var renderedMail = await MakeRenderedMail(request);
+        var message = await MakeMessage(request);
+
+        await client.SendAsync(message);
+
+        return Ok();
+    }
+
+    private async Task<MailMessage> MakeMessage(SendSampleMailRequest request)
+    {
+        var model = new SampleModel(request.Salutation);
+
+        const string view = "Sample";
+        var renderedMail = await emailRenderer.RenderAsync(view, model);
 
         var message = new MailMessage
         {
@@ -47,16 +49,6 @@ public class MailController(
             },
         };
 
-        await client.SendAsync(message);
-
-        return Ok();
-    }
-
-    private async Task<MailContent> MakeRenderedMail(SendSampleMailRequest request)
-    {
-        const string view = "Sample";
-        var model = new SampleModel(request.Salutation);
-        var renderedMail = await emailRenderer.RenderAsync(view, model);
-        return renderedMail;
+        return message;
     }
 }
