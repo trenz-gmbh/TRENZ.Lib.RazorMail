b9374c82f6342c910393b47e19d5d096c3770435
Sören Kuklau <>
2024-10-14 16:45:43 +0200

    link documentation for `Importance` and `X-Priority`

--- TRENZ.Lib.RazorMail.Core/Models/MailImportance.cs
+++ TRENZ.Lib.RazorMail.Core/Models/MailImportance.cs
@@ -4,8 +4,17 @@ namespace TRENZ.Lib.RazorMail.Models;
 /// Specifies the urgency in which the mail should be delivered.
 /// </summary>
 /// <remarks>
+/// <para>
 /// This sets both the <c>Importance</c> and <c>X-Priority</c> headers, for
 /// broad compatibility.
+/// </para>
+/// <para>
+/// RFC 2156 (https://www.rfc-editor.org/rfc/rfc2156) defines <c>Importance</c>
+/// values <c>low</c>, <c>normal</c>, and <c>high</c>. <c>X-Priority</c> is
+/// proprietary, and documented at Microsoft:
+/// https://learn.microsoft.com/en-us/openspecs/exchange_server_protocols/ms-oxcmail/2bb19f1b-b35e-4966-b1cb-1afd044e83ab,
+/// and takes the values <c>1</c> (highest) through <c>5</c> (lowest).
+/// </para>
 /// </remarks>
 public enum MailImportance
 {
